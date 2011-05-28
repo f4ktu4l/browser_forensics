f78f682a654b59bd2356b1b041d76ad9da371a21
Alex Krepelka <f4ktu4l@securethemasses.com>
2011-05-28 02:07:56 -0700

    Edited src/firefox_forensic.py via GitHub

--- src/firefox_forensic.py
+++ src/firefox_forensic.py
@@ -3,7 +3,7 @@ Created on May 3, 2011
 
 @author: alex
 '''
-import sqlite3,datetime
+import sqlite3,datetime,re
 
 class firefox_forensic(object):
 
@@ -31,4 +31,48 @@ class firefox_forensic(object):
         for row in c:
             print('Visited: ' + row[1].encode('utf-8'))
         c.close()
+    
+    def get_downloads(self,download_file):
+        downloads = sqlite3.connect(download_file)
+        c = downloads.cursor()
+        c.execute('select name,source,target,startTime,endTime from moz_downloads')
+        for row in c:
+                print('Download Name: ' + row[0].encode('utf-8'))
+                print('URL: ' + row[1].encode('utf-8'))
+                print('Location: ' + row[2].encode('utf-8'))
+                print('Start: ' + str(datetime.datetime.fromtimestamp(row[3]/1e6)) + ' End: ' + str(datetime.datetime.fromtimestamp(row[4]/1e6)))
+                print()
+                
+    def get_facebook(self,history_file):
+        history = sqlite3.connect(history_file)
+        c = history.cursor()
+        c.execute('select url from moz_places')
+        pat = re.compile('facebook.com\/profile.php\?id\=')
+        for row in c:
+            if pat.search(''.join(row)):
+                print(pat.search(''.join(row))).string
+        print("Ending Facebook!")
+        c.close()
+            
+    def get_form_history(self,form_history):
+        forms = sqlite3.connect(form_history)
+        c = forms.cursor()
+        c.execute('select fieldname,value,timesUsed from moz_formhistory')
+        sorted = []
         
+        for row in c:
+            temp = str(row[2]),row[0].encode('utf-8'),row[1].encode('utf-8')
+            sorted.append(temp)
+        
+        sorted.sort()    
+        sorted.reverse()
+        for i in sorted:
+            print(i)
+    
+    def get_bookmarks(self,bookmarks_folder):
+        pass
+    
+    def get_signons(self,signons_history):
+        signons = sqlite3.connect(signons_history)
+        c = signons.cursor()
+        c.execute()
